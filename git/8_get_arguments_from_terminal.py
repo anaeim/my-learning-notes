79377b2cc312a14db9bfc1301657c7cc3c8b370d
ali <ali.naeim.gdrive@gmail.com>
2022-07-30 10:35:38 -0600

    add git notes

---
+++ git/8_get_arguments_from_terminal.py
@@ -0,0 +1,10 @@
+import sys
+
+print(sys.argv)
+# sys.argv be shoma ye list bermigardune az argument haiy ke ma be onvan be input be python tuye terminal dadim
+# hala avalish mishe esm e file badi hash ham mishe baghie ye argument ha
+
+print('the 2nd argument:')
+print(sys.argv[2])
+
+# argparse ye library e takhasosi tar hast baraye dadan e argument be python. ba un mishe flag(--) ham masalan dad be python
