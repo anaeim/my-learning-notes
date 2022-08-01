cf2b0f1f86b340052816261ac4e50d8623a74cc2
ali <ali.naeim.gdrive@gmail.com>
2022-08-01 09:21:30 -0600

    add new notes to git dir

--- git/8_get_arguments_from_terminal.py
+++ git/8_get_arguments_from_terminal.py
@@ -1,10 +1,16 @@
+# using sys library you can give the .py file input arguments like a list
+# example of command in terminal:
+# python 8_get_arguments_from_terminal.py ali hasan reza hossein 12 15
+
+# here sys just gets positional inout arguments
+# you use argparse to give flags (--) and option (-) as the input to the file
+
+
 import sys
 
 print(sys.argv)
 # sys.argv be shoma ye list bermigardune az argument haiy ke ma be onvan be input be python tuye terminal dadim
 # hala avalish mishe esm e file badi hash ham mishe baghie ye argument ha
 
-print('the 2nd argument:')
-print(sys.argv[2])
-
-# argparse ye library e takhasosi tar hast baraye dadan e argument be python. ba un mishe flag(--) ham masalan dad be python
+# print('the 2nd argument:')
+# print(sys.argv[2])
